ef1f2b2f24544e8baaab6dab711487313a904462
xwy <845619585@qq.com>
2019-01-15 23:12:36 +0800

    new Structure

---
+++ src/main/java/xwy/proxy/cglib/Xwy.java
@@ -0,0 +1,9 @@
+package xwy.proxy.cglib;
+
+public class Xwy {
+	
+	private void findLove() {
+		System.out.println("肤白貌美大长腿");
+	}
+
+}
---
+++ src/main/java/xwy/proxy/cglib/XwyMeiPo.java
@@ -0,0 +1,18 @@
+package xwy.proxy.cglib;
+
+import java.lang.reflect.Method;
+
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+public class XwyMeiPo implements MethodInterceptor {
+
+	@Override
+	public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
